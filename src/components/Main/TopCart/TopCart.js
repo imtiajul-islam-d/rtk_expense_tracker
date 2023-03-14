9d413c6e0335bf37636cbbb472b81f2bea642481
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-14 14:36:09 +0600

    balance calculation added

--- src/components/Main/TopCart/TopCart.js
+++ src/components/Main/TopCart/TopCart.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import numberWithCommas from "../../../utils/thousandSeparators";
 
 const TopCart = () => {
+  const { transactions } = useSelector((state) => state.transaction);
+  const calculate = (transactions) => {
+    let balance = 0;
+    transactions.forEach((t) => {
+      const { type, amount } = t;
+      if (type === "income") {
+        balance += amount;
+      } else {
+        balance -= amount;
+      }
+    });
+    return balance;
+  };
+
   return (
     <>
       <div className="top_card">
         <p>Your Current Balance</p>
         <h3>
-          <span>৳</span>
-          <span>10500</span>
+          <span>৳ </span>
+          {transactions?.length > 0 ? (
+            <span>{numberWithCommas(calculate(transactions))}</span>
+          ) : (
+            <span>0</span>
+          )}
         </h3>
       </div>
     </>
